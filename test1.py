3e33bcba3f4171df79dbe5f14a3bbaff9f374090
Matveev Evgeniy <doc.fouks@yandex.ru>
2020-07-04 22:06:27 +0300

    first commit

---
+++ test1.py
@@ -0,0 +1,25 @@
+a = 1
+b = 3
+c = a + b
+
+print (c)
+
+d = 'Привет!'
+e = 'Как дела?'
+f = d + ' ' + e
+
+print (f)
+
+print (d + ' ' + str(a))
+print ('Кудык! {} {}'.format(d, a))
+print ('Бултых! {d} {a}'.format(d=d, a=a))
+
+
+g = 5<0
+print (g)
+
+x = int(input('Введите число: '))
+if x>1 and x<5:
+    print ('Игорь молодец!')
+else:
+    print ('Женя молодец!')
